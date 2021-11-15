bd9d3eead77a7389c59d07e2f9bb1094c127e9bb
zeb-roshan <roshan.n@zebpay.com>
2021-11-15 12:24:22 +0530

    update listenning

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/libp2p/go-libp2p"
 )
@@ -12,7 +15,14 @@ func main() {
 	ctx := context.Background()
 
 	// start a libp2p node with default settings
-	node, err := libp2p.New(ctx)
+	node, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2000"))
+
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+
 	if err != nil {
 		panic(err)
 	}
