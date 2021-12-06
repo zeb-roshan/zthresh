bd7230ad5da4f9be3efa486fe96238b6d622bb07
zeb-roshan <89435901+zeb-roshan@users.noreply.github.com>
2021-12-06 14:45:09 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -3,74 +3,172 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"math/rand"
+	"reflect"
+	"strconv"
+	"time"
+
+	"sync"
 
 	"github.com/libp2p/go-libp2p"
-	peerstore "github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
-	multiaddr "github.com/multiformats/go-multiaddr"
+	crypto "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	discovery "github.com/libp2p/go-libp2p-discovery"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	libp2ptls "github.com/libp2p/go-libp2p-tls"
 )
 
+//var fmt = log.fmt("rendezvous")
+
+func handleStream(stream network.Stream) {
+	fmt.Println("Got a new stream!")
+	fmt.Println("Connect with ", stream.ID())
+	// Create a buffer stream for non blocking read and write.
+	//rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+	/*
+		go readData(rw, stream)
+		go writeData(rw, stream)
+	*/
+	// 'stream' will stay open until you close it (or the other side closes it).
+}
+
 func main() {
-	// create a background context (i.e. one that never cancels)
-	ctx := context.Background()
 
-	// start a libp2p node that listens on a random local TCP port,
-	// but without running the built-in ping protocol
-	node, err := libp2p.New(ctx,
-		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
-		libp2p.Ping(false),
+	//input threshold, name and user
+	//fmt.Println("Enter threshold:")
+	/* var input_peers int
+	fmt.Scanln(&input_peers)
+	for input_peers < 2 {
+		fmt.Println("Threshold cannot be less than 2, Please re-enter")
+		fmt.Scanln(&input_peers)
+	} */
+	fmt.Println(rand.Intn(9999-1000) + 1000)
+
+	port := strconv.Itoa(rand.Intn(9999-1000) + 1000)
+	//fmt.Println("Port: ", reflect.TypeOf(port))
+
+	//TCP connection
+	priv, _, _ := crypto.GenerateKeyPair(crypto.Secp256k1, 256)
+	h, _ := libp2p.New(
+		//context.Background(),
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/"+string(port)),
+		libp2p.Security(libp2ptls.ID, libp2ptls.New),
+		libp2p.Identity(priv),
 	)
+
+	ctx := context.Background()
+	h.SetStreamHandler(protocol.ID("tss/1"), handleStream)
+	kademliaDHT, err := dht.New(cont(), h)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("type is ", h.Addrs(), h.ID().String())
+	// Bootstrap the DHT. In the default configuration, this spawns a Background
+	// thread that will refresh the peer table every five minutes.
+	fmt.Println("Bootstrapping the DHT")
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+
+	// Let's connect to the bootstrap nodes first. They will tell us about the
+	// other nodes in the network.
+	var wg sync.WaitGroup
+	for _, peerAddr := range dht.DefaultBootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := h.Connect(ctx, *peerinfo); err != nil {
+				fmt.Println(err)
+			} else {
+				//fmt.Println("Connection established with bootstrap node:", *peerinfo)
+				return
+			}
+		}()
+	}
+	wg.Wait()
+
+	// We use a rendezvous point "meet me here" to announce our location.
+	// This is like telling your friends to meet you at the Eiffel Tower.
+	fmt.Println("Announcing ourselves...")
+	routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
+	discovery.Advertise(cont(), routingDiscovery, "base")
+	fmt.Println("Successfully announced!")
+
+	// Now, look for others who have announced
+	// This is like your friend telling you the location to meet you.
+	fmt.Println("Searching for other peers...")
+	peerChan, err := routingDiscovery.FindPeers(cont(), "base")
 	if err != nil {
 		panic(err)
 	}
 
-	// configure our own ping protocol
-	pingService := &ping.PingService{Host: node}
+	for peer := range peerChan {
 
-	// print the node's PeerInfo in multiaddr format
-	peerInfo := peerstore.AddrInfo{
-		ID:    node.ID(),
-		Addrs: node.Addrs(),
-	}
-	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("libp2p node address:", addrs[0])
-	fmt.Println(node.ID(), node.Addrs(), os.Args)
-	// if a remote peer has been passed on the command line, connect to it
-	// and send it 5 ping messages, otherwise wait for a signal to stop
-	if len(os.Args) > 1 {
-		addr, err := multiaddr.NewMultiaddr(os.Args[1])
+		if peer.ID == h.ID() {
+			fmt.Println("And here ", reflect.TypeOf(h.ID), reflect.TypeOf(peer.ID))
+
+			continue
+		}
+
+		_, err := h.NewStream(cont(), peer.ID, protocol.ID("tss/1"))
+
 		if err != nil {
-			panic(err)
+			//fmt.Println("Connection failed:", err)
+			continue
+		} else {
+			fmt.Println("Found ", peer)
+			fmt.Println("Connecting to:", peer.Addrs[0].String()+"/p2p/"+peer.ID.String())
+			/* rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+			fmt.Println(rw) */
+			peerlist = append(peerlist, peers{id: len(peerlist) + 1, addr: peer.Addrs[0].String() + "/p2p/" + peer.ID.String(), connect: false})
+			peerattempt = append(peerattempt, peer.ID.String())
+
+			go attemptconnection(len(peerlist) + 1)
+			/*
+				go writeData(rw, stream)
+				go readData(rw, stream) */
 		}
-		peer, err := peerstore.AddrInfoFromP2pAddr(addr)
-		if err != nil {
-			panic(err)
-		}
-		if err := node.Connect(ctx, *peer); err != nil {
-			panic(err)
-		}
-		fmt.Println("sending 5 ping messages to", addr)
-		ch := pingService.Ping(ctx, peer.ID)
-		for i := 0; i < 5; i++ {
-			res := <-ch
-			fmt.Println("pinged", addr, "in", res.RTT)
-		}
-	} else {
-		// wait for a SIGINT or SIGTERM signal
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-		<-ch
-		fmt.Println("Received signal, shutting down...")
+
+		//fmt.Println("Connected to:", peer)
 	}
 
-	// shut the node down
-	if err := node.Close(); err != nil {
-		panic(err)
+	go handle_write()
+	select {}
+}
+
+func cont() context.Context {
+
+	return context.Background()
+}
+
+//var peers []string
+type peers struct {
+	id      int
+	addr    string
+	connect bool
+}
+
+var peerlist []peers
+
+var peerattempt []string
+
+func handle_write() {
+	for {
+		time.Sleep(10 * time.Second)
+		fmt.Println(peerlist)
 	}
+}
+
+func attemptconnection(id int) {
+
+	fmt.Println("Attempting connection")
+
+	//pass code entry
+
+	//Connect and show proof - Add keccak auth?
+
 }
